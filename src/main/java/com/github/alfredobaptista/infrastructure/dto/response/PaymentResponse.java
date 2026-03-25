280603d61f150305547ad524306d487b24f2cdb6
Alfredo <baptistaalfredo81@gmail.com>
2026-03-25 13:34:44 +0100

    refactor: update payment response

--- src/main/java/com/github/alfredobaptista/infrastructure/dto/response/PaymentResponse.java
+++ src/main/java/com/github/alfredobaptista/infrastructure/dto/response/PaymentResponse.java
@@ -1,8 +1,11 @@
 package com.github.alfredobaptista.infrastructure.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record PaymentResponse(
         String id,
         BigDecimal amount,
